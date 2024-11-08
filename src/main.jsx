953e72f7bec56584224c0f58bd9fb3de76868411
Rajat Sachdeva <r.sachdeva3105@gmail.com>
2024-11-08 11:52:30 -0500

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider";
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <BrowserRouter>
-        <AuthProvider>
+      <AuthProvider>
+        <HashRouter>
           <App />
-        </AuthProvider>
-      </BrowserRouter>
+        </HashRouter>
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>
 );
